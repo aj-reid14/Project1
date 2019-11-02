8c77cc9562cb36704f162f8e38453d00efaa2f32
ajreid <axr490@miami.edu>
2019-11-02 10:36:45 -0400

    added initial code for .js file and started on-click for button (level 2)

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function()
+{
+    ConfigureButtons();
+})
+
+function ConfigureButtons()
+{
+    // Level 1 Button Code
+
+    // -----------------------
+
+
+
+    // Level 2 Button Code
+    $("#level2").click(function()
+    {
+        
+    })
+    // -----------------------
+
+
+
+    // Level 3 Button Code
+    
+    // -----------------------
+}
