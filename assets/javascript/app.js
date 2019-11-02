4da8d3667504d35c81158ae7f068ceacacee5821
Marion <gallula2427@gmail.com>
2019-11-02 11:40:06 -0400

    show and hide pages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,45 @@
-$(document).ready(function()
-{
+$(document).ready(function () {
     ConfigureButtons();
-})
 
-function ConfigureButtons()
-{
-    // Level 1 Button Code
-
-    // -----------------------
-
-
-
-    // Level 2 Button Code
-    $("#level2").click(function()
-    {
-        
-    })
-    // -----------------------
-
-
-
-    // Level 3 Button Code
+    function ConfigureButtons() {
     
-    // -----------------------
-}
+
+        // Level 1 Button Code
+
+        // -----------------------
+
+
+
+        // Level 2 Button Code
+        $("#level2").click(function () {
+
+        })
+        // -----------------------
+
+
+
+        // Level 3 Button Code
+
+        // -----------------------
+    }
+
+    //On click button start questions appear
+    $("#level1").click(function () {
+        $("#start-page").hide();
+        $("#joke-page").show();
+
+    });
+
+    $("#level2").click(function () {
+        $("#start-page").hide();
+        $("#media-page").show();
+
+    });
+
+    $("#level3").click(function () {
+        $("#start-page").hide();
+        $("#event-page").show();
+
+    });
+
+});
