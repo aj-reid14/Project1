c9319e8d54dfe131df40ebe0eb48c06124a099fd
ajreid <axr490@miami.edu>
2019-11-09 01:29:58 -0500

    added firebase (timesClicked) to bored ball and updated styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCEe4cKSqbkoj9DGmB2dPM6xTfTMrnCKcM",
+    authDomain: "project-1-bored-board.firebaseapp.com",
+    databaseURL: "https://project-1-bored-board.firebaseio.com",
+    projectId: "project-1-bored-board",
+    storageBucket: "project-1-bored-board.appspot.com",
+    messagingSenderId: "934204858779",
+    appId: "1:934204858779:web:5bb0a29eba5bf9f9a1413e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+let database = firebase.database();
+
 let colorVals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
     "A", "B", "C", "D", "E", "F"]
 
@@ -5,139 +18,166 @@ $(document).ready(function () {
     ConfigureButtons();
     gapi.load("client");
 
-    function ConfigureButtons() {            
-
-        // "Bored Ball" on-click
-        $("#bored-ball").click(function() {
-            let newColor = "#";
-            let color_r = Math.floor(Math.random() * 256);
-            let color_g = Math.floor(Math.random() * 256);
-            let color_b = Math.floor(Math.random() * 256);
-            
-            $("#bored-ball").css("background-color", "rgba(" + color_r + ", " +  color_g + ", " + color_b + ", " +  0.8 + ")");
-        })
-
-
-
-        // Level 1 Button Code
-        $("#level1").click(function () {
-            $("#start-page").hide();
-            $("#joke-page").show();
-                            
-            let giphyURL = "https://official-joke-api.appspot.com/random_ten";
-                
-            $.ajax({
-                url: giphyURL,
-                method: "GET"
-            })
-                .then(function(response) {
-                console.log(response)
-                
-                let jokeSetup = $("<div>" + response[1].setup + "</div>");
-                $("#jokeSetup").html(jokeSetup);
-                
-                let jokeDelivery = $("<div>" + response[1].punchline + "</div>");
-                $("#jokeDelivery").html(jokeDelivery);
-                console.log(response[1].punchline);
-            });
-    
-            let jokeGIFapi = "5aq9ySNcbmDa3eP7R5B0OdOaJBvYihlY"
-            let jokeGIFurl = "https://api.giphy.com/v1/gifs/random?api_key=" + jokeGIFapi;
-    
-            $.ajax({
-                url: jokeGIFurl,
-                method: "GET"
-            }).then(function (response) {
-                let newDIV = $("<div>");
-                console.log(response);
-                
-                let newGIF = $("<img>");
-                newGIF.addClass("gif");
-                newGIF.attr("src", response.data.image_original_url);
-                
-                newDIV.append(newGIF);
-                $("#jokeGIF").html(newDIV);
-                
-            })
-
-        })
-
-        // -----------------------
-
-    $("#level2").click(function () {
-
-        $("#start-page").hide();
-        $("#media-page").show();
-        
-        loadClient();
+    database.ref().on("value", function(snapshot) {
+        database.ref().set({timesClicked: snapshot.val().timesClicked})
+        $("#bored-text").text(snapshot.val().timesClicked);
     })
-  
-    // Level 3 Button Code
-    $("#level3").click(function() {
+});
 
+function ConfigureButtons() {            
+
+    // "Bored Ball" on-click
+    $("#bored-ball").click(function() {
+
+        let color_r;
+        let color_g;
+        let color_b;
+        
+        database.ref().once("value", function(snapshot) {
+            
+            let totalClicks = 1 + snapshot.val().timesClicked;
+            database.ref().set({timesClicked: totalClicks});
+            
+            $("#bored-text").animate({fontSize: "28px"}, {
+                duration: 75,
+                easing: "linear",
+                done: function(){
+                    $("#bored-text").animate({fontSize: "14px"}, {duration: 75, easing: "linear"});
+                }
+            })
+            
+            if ((totalClicks % 25) === 0) {
+                let newColor = "#";
+                for (let i = 0; i < 6; i++) {
+                    newColor += colorVals[Math.floor(Math.random() * colorVals.length)];
+                }
+                console.log(newColor);
+                $("#bored-text").css("color", newColor);
+            }
+
+        })
+        color_r = Math.floor(Math.random() * 256);
+        color_g = Math.floor(Math.random() * 256);
+        color_b = Math.floor(Math.random() * 256);
+                
+        $("#bored-ball").css("background-color", "rgba(" + color_r + ", " +  color_g + ", " + color_b + ", " +  0.8 + ")");
+    })
+
+    // Level 1 Button Code
+    $("#level1").click(function () {
         $("#start-page").hide();
-        $("#event-page").show();
+        $("#joke-page").show();
+                        
+        let giphyURL = "https://official-joke-api.appspot.com/random_ten";
+            
+        $.ajax({
+            url: giphyURL,
+            method: "GET"
+        })
+            .then(function(response) {
+            // console.log(response)
+            
+            let jokeSetup = $("<div>" + response[1].setup + "</div>");
+            $("#jokeSetup").html(jokeSetup);
+            
+            let jokeDelivery = $("<div>" + response[1].punchline + "</div>");
+            $("#jokeDelivery").html(jokeDelivery);
+            // console.log(response[1].punchline);
+        });
+
+        let jokeGIFapi = "5aq9ySNcbmDa3eP7R5B0OdOaJBvYihlY"
+        let jokeGIFurl = "https://api.giphy.com/v1/gifs/random?api_key=" + jokeGIFapi;
 
         $.ajax({
-            url: "http://app.ticketmaster.com/discovery/v2/events.json?apikey=8WUUFHzUOAX0dQ43PlH9MnGXOQanNz4D&locale=*&city=Miami&size=200",    
+            url: jokeGIFurl,
             method: "GET"
-        }).then(function(response)
-        {
-        
-        console.log(response);
+        }).then(function (response) {
+            let newDIV = $("<div>");
+            // console.log(response);
+            
+            let newGIF = $("<img>");
+            newGIF.addClass("media");
+            newGIF.attr("src", response.data.image_original_url);
+            
+            newDIV.append(newGIF);
+            $("#jokeGIF").html(newDIV);
+            
+        })
 
-        let randomNumber = Math.floor(Math.random() * response._embedded.events.length);
-
-        let randomEvent = response._embedded.events[randomNumber].url;
-        console.log(randomEvent)
-        $("#link").attr("href", randomEvent);
-
-        let eventName = response._embedded.events[randomNumber].name;
-        $("#link").text(eventName);
-        console.log(eventName);
-    
-        let date = response._embedded.events[randomNumber].dates.start.localDate;
-        $("#eventDate").text(date);
-        console.log(date);
-
-        let img = response._embedded.events[randomNumber].images[4].url;
-        $("#eventIMG").attr("src", img);
-        console.log(img);
-        });
-    }); 
-
-    $("#eventBtn").click(function(){
-        $("#level3").click()
-    });
-    
-       
-
-    
-
-    $("#media-gif").click(function() {
-        SearchGIF();
     })
 
-    $("#media-video").click(function() {
-        SearchVideo();
-    })
-    //On click back-button
-    $("#go-back1").click (function () {
-    $("#start-page").show(); 
-    $("#joke-page").hide();
-    });
+    // -----------------------
 
-    $("#go-back2").click (function () {
-    $("#start-page").show(); 
-    $("#media-page").hide();
-    });
+$("#level2").click(function () {
 
-    $("#go-back3").click (function () {
-    $("#start-page").show(); 
-    $("#event-page").hide();
+    $("#start-page").hide();
+    $("#media-page").show();
+    
+    loadClient();
+})
+
+// Level 3 Button Code
+$("#level3").click(function() {
+
+    $("#start-page").hide();
+    $("#event-page").show();
+
+    $.ajax({
+        url: "http://app.ticketmaster.com/discovery/v2/events.json?apikey=8WUUFHzUOAX0dQ43PlH9MnGXOQanNz4D&locale=*&city=Miami&size=200",    
+        method: "GET"
+    }).then(function(response)
+    {
+    
+    // console.log(response);
+
+    let randomNumber = Math.floor(Math.random() * response._embedded.events.length);
+
+    let randomEvent = response._embedded.events[randomNumber].url;
+    // console.log(randomEvent)
+    $("#link").attr("href", randomEvent);
+
+    let eventName = response._embedded.events[randomNumber].name;
+    $("#link").text(eventName);
+    // console.log(eventName);
+
+    let date = response._embedded.events[randomNumber].dates.start.localDate;
+    $("#eventDate").text(date);
+    // console.log(date);
+
+    let img = response._embedded.events[randomNumber].images[4].url;
+    $("#eventIMG").addClass("media");
+    $("#eventIMG").attr("src", img);
+    // console.log(img);
     });
-    }
+}); 
+
+$("#eventBtn").click(function(){
+    $("#level3").click()
 });
+
+$("#media-gif").click(function() {
+    SearchGIF();
+})
+
+$("#media-video").click(function() {
+    SearchVideo();
+})
+//On click back-button
+$("#go-back1").click (function () {
+$("#start-page").show(); 
+$("#joke-page").hide();
+});
+
+$("#go-back2").click (function () {
+$("#start-page").show(); 
+$("#media-page").hide();
+});
+
+$("#go-back3").click (function () {
+$("#start-page").show(); 
+$("#event-page").hide();
+});
+}
 
 function loadClient() {
     let youtubeAPI = "AIzaSyBMblqCRc-6Ddlvc3mHpkgxcLUAy6HSezs";
@@ -162,6 +202,7 @@ function SearchVideo() {
         let videoLink = "https://www.youtube.com/embed/" + response.result.items[randomResult].id.videoId;
 
         let newVideo = $("<iframe frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>");
+        newVideo.addClass("media");
         newVideo.attr("width", screen.width * 0.3);
         newVideo.attr("height", screen.height * 0.3);
         newVideo.attr("src", videoLink);
@@ -182,7 +223,7 @@ function SearchGIF() {
     }).then(function(response)
     {        
         let newGIF = $("<img>");
-        newGIF.addClass("gif");
+        newGIF.addClass("media");
         newGIF.attr("src", response.data.image_original_url);
         
         $("#media-appear-here").html(newGIF);                
